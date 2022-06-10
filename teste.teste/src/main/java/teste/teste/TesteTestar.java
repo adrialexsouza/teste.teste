e6101d125c246e3d8f16af4641443ef2e9722bb8
Adriana Abrantes <adrialexsouza@gmail.com>
2022-06-09 23:33:10 -0300

    upload

---
+++ teste.teste/src/main/java/teste/teste/TesteTestar.java
@@ -0,0 +1,12 @@
+package teste.teste;
+
+public class TesteTestar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ola mundo");
+
+	}
+
+}
